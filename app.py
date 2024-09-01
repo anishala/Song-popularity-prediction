db49b7e64c11bff5d04f43e53fa4a26d84ec86f3
anishala <108260043+anishala@users.noreply.github.com>
2024-09-01 20:27:15 +0530

    Flask app

--- app.py
+++ app.py
@@ -74,4 +74,4 @@ def predict():
         return jsonify({"error": f"An error occurred: {str(e)}"}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0",port=5000,debug=True)
